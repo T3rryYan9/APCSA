e6c04905c1c8bf0be5b78acefc39f6119dc80b32
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 17:07:53 -0400

    Create Main.java

---
+++ Lessons/(25)Polymorphism/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+// Polymorphism 
+    Soup soup = new Soup();
+    Taco taco = new Taco();
+    Rice rice = new Rice();
+
+    soup.eat();
+    taco.eat();
+    rice.eat();
+
+// Dynamic Polymorphism 
+    Scanner scanner = new Scanner(System.in);
+    Food food;
+    System.out.println("What do you want to eat");
+    System.out.println("Type 1 for Soup, Type 2 for Taco, Type 3 for Rice ");
+    int choice = scanner.nextInt();
+    if(choice==1){
+      food = new Soup();
+      food.eat();
+    } else if(choice ==2){
+      food = new Taco();
+      food.eat();
+    } else if (choice ==3){
+      food = new Rice();
+      food.eat();
+    } else {
+      System.out.println("That was not a choice silly");
+    }
+    
+  }
+}
