92944d70d8da82ebc57ff2a01f6b7e8efd8adbe0
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 16:43:04 -0400

    Create Vehicle.java

---
+++ Lessons/(18)Inheritance/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle {
+  double speed; //Variable (Atrribute)
+  // Go Method
+  static void go() {
+    System.out.println("Go");
+  }
+  // Stop Method
+  void stop(){
+    System.out.println("Stop");
+  }
+}
