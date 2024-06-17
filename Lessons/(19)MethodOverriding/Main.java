36f880667e18d5388a3c6a3d2d7b18275cad3c38
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 16:44:04 -0400

    Create Main.java

---
+++ Lessons/(19)MethodOverriding/Main.java
@@ -0,0 +1,10 @@
+//Method Overriding - Delcaring a method in subclass which is already present in the parent class. This is done so a child class has its own implamentations. 
+
+public class Main {
+  public static void main(String[] args) {
+//Creating Object
+    Dog dog = new Dog();
+    dog.speak();
+    
+  }
+}
