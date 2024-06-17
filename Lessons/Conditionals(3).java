e368d2f505ffff1ac1f5720f0f9d483e43deda01
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 16:32:29 -0400

    Create Conditionals(3).java

---
+++ Lessons/Conditionals(3).java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+   // double people1 = 1000;
+   // double people2 = 100;
+  //  System.out.println(people2>=15 && people1<=20);
+  
+    int thisIsANumber = 25;
+    int thisIsANumberToo = 10;
+    boolean idk = thisIsANumber>= 25 && thisIsANumberToo>=15;
+    System.out.println(idk);
+  }
+}
