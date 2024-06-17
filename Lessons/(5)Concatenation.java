da5256567b805289ef9af9a1f55ea47e95e937eb
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 16:34:15 -0400

    Create (5)Concatenation.java

---
+++ Lessons/(5)Concatenation.java
@@ -0,0 +1,17 @@
+class Main {
+  static String greeting2 = "Terry";
+  
+  public static void main(String[] args) {
+    String name1 = "Mr. Ashong";
+    String name2 = "Mr. Ashong";
+
+    String name3 = new String ("Mr. Ashong");
+    String name4 = new String ("Mr. Ashong");
+
+    String greeting = "Hello, you are ";
+   
+      
+    
+    System.out.println(greeting + greeting2);
+  }
+  }
