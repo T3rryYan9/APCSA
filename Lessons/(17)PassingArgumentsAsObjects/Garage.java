0f677b78248a9c90d7a5468dacd30b76672008ca
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 16:42:21 -0400

    Create Garage.java

---
+++ Lessons/(17)PassingArgumentsAsObjects/Garage.java
@@ -0,0 +1,6 @@
+public class Garage{
+// Void is used because it doesn't need to return anything. 
+  void park(Car car){
+    System.out.println("The " + car.name + " is parked in this garage.");
+  }
+}
