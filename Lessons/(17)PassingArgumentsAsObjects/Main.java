b98ad8c5267dda2c309f84cc8b59300ad32c778d
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 16:42:00 -0400

    Create Main.java

---
+++ Lessons/(17)PassingArgumentsAsObjects/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+  //Garage Object
+  Garage garage = new Garage();
+  // Car Object
+  Car car = new Car("Mercedes");
+
+  garage.park(car);
+  }
+}
